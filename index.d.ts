59b12e00d948ed7ff5fbd04d419fe8b34785782d
danibonilha <dani.bonilha@live.com>
2025-01-23 18:00:25 -0300

    [chore]: Add typescript

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+export interface BackupFile {
+  name: string;
+  id: string;
+  path: string; // iOS only
+  lastModified: string;
+}
+
+export interface ListFilesResult {
+  files: BackupFile[];
+}
+
+interface RNCloudFSMethods {
+  logout(): Promise<void>;
+  loginIfNeeded(): Promise<void>;
+  listFiles(listFilesParams: {
+    scope: String;
+    targetPath: string;
+  }): Promise<ListFilesResult>;
+  deleteFromCloud(file: BackupFile): Promise<void>;
+  copyToCloud(copyToCloudParams: {
+    mimeType: string;
+    scope: string;
+    sourcePath: { path: string };
+    targetPath: string;
+  });
+  fileExists(fileExistsParam: {
+    scope: string;
+    targetPath?: string;
+    fileId?: string;
+  }): Promise<boolean>;
+  getIcloudDocument(filename: string): Promise<string>; // iOS only
+  getGoogleDriveDocument(id: string): Promise<string>; // android only
+  syncCloud(): Promise<boolean>; // iOS only
+  isAvailable(): Promise<boolean>; // iOS only
+}
+
+declare const RNCloudFS: RNCloudFSMethods;
+export default RNCloudFS;
